b59a64e453d82338b67ea144ea4382c2b351fcda
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-08-12 15:55:00 +0200

    Removed redundant code

--- test/unit.py
+++ test/unit.py
@@ -1,15 +1,11 @@
 import os
 import unittest
-from os.path import expanduser
-
 from util.Docker import Docker
-
 
 class GoServices(unittest.TestCase):
     def test_go(self):
         script_dir = os.path.dirname(os.path.realpath(__file__))
         code_dir = script_dir + "/.."
-        home = expanduser("~")
         goPath = os.environ['GOPATH']
         command = ['docker', 'run', '--rm', '-v', goPath + ':/go/bin/', '-v', code_dir + ':/usr/src/', '-w',
                    '/usr/src/', '-e', 'GOPATH=/go/bin', 'golang:1.6', 'go', 'test', '-v']
