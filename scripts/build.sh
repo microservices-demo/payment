1eea648d5d943d31bb426f68c921b3ea502167c6
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-08-12 12:42:42 +0200

    Fixes build issues with the docker/ directories

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,13 +22,13 @@ fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
-cp -r $CODE_DIR/cmd/ $CODE_DIR/docker/payment/cmd/
-cp $CODE_DIR/*.go $CODE_DIR/docker/payment/
+cp -r $CODE_DIR/cmd/ $CODE_DIR/docker/paymentsvc/cmd/
+cp $CODE_DIR/*.go $CODE_DIR/docker/paymentsvc/
 
 REPO=${GROUP}/$(basename payment);
 
-$DOCKER_CMD build -t ${REPO}-dev $CODE_DIR/docker/payment;
+$DOCKER_CMD build -t ${REPO}-dev $CODE_DIR/docker/paymentsvc;
 $DOCKER_CMD create --name payment ${REPO}-dev;
-$DOCKER_CMD cp payment:/app/main $CODE_DIR/docker/payment/app;
+$DOCKER_CMD cp payment:/app/main $CODE_DIR/docker/paymentsvc/app;
 $DOCKER_CMD rm payment;
-$DOCKER_CMD build -t ${REPO}:${COMMIT} -f $CODE_DIR/docker/payment/Dockerfile-release $CODE_DIR/docker/payment;
+$DOCKER_CMD build -t ${REPO}:${COMMIT} -f $CODE_DIR/docker/paymentsvc/Dockerfile-release $CODE_DIR/docker/paymentsvc;
