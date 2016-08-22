bad2c0e4f1856b5eed3618e9287e64258e212d20
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-08-22 11:24:42 +0200

    Removing redundant method calls

--- api-spec/hooks.js
+++ api-spec/hooks.js
@@ -1,19 +1,6 @@
 const hooks = require('hooks');
 
 // Setup database connection before Dredd starts testing
-hooks.beforeAll((transactions, done) => {
-    done();
-});
-
-// Close database connection after Dredd finishes testing
-hooks.afterAll((transactions, done) => {
-    done();   
-});
-
-hooks.beforeEach((transaction, done) => {
-    done();
-});
-
 hooks.before("/paymentAuth > POST", function(transaction, done) {
     transaction.request.headers['Content-Type'] = 'application/json';
     transaction.request.body = JSON.stringify({
