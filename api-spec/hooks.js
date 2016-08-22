eddec93d6427e049f370064bfd2a106a417dcfaf
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-08-22 10:39:23 +0200

    Fixing api spec and test

--- api-spec/hooks.js
+++ api-spec/hooks.js
@@ -1,10 +1,7 @@
 const hooks = require('hooks');
 
-
-
 // Setup database connection before Dredd starts testing
 hooks.beforeAll((transactions, done) => {
-
     done();
 });
 
@@ -14,5 +11,13 @@ hooks.afterAll((transactions, done) => {
 });
 
 hooks.beforeEach((transaction, done) => {
+    done();
+});
+
+hooks.before("/paymentAuth > POST", function(transaction, done) {
+    transaction.request.headers['Content-Type'] = 'application/json';
+    transaction.request.body = JSON.stringify({
+	"amount": 10.00
+    });
     done();
 });
