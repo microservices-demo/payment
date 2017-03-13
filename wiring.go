8bc59f8c3bd4db80df3989c362e08d26eb5c36e2
Phil Winder <phil@winderresearch.com>
2017-03-13 14:50:09 +0000

    Remove old monitoring code.

--- wiring.go
+++ wiring.go
@@ -7,10 +7,8 @@ import (
 	"github.com/go-kit/kit/log"
 	"golang.org/x/net/context"
 
-	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-	stdopentracing "github.com/opentracing/opentracing-go"
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"github.com/microservices-demo/payment/middleware"
+	stdopentracing "github.com/opentracing/opentracing-go"
 )
 
 func WireUp(ctx context.Context, declineAmount float32, tracer stdopentracing.Tracer, serviceName string) (http.Handler, log.Logger) {
@@ -22,29 +20,11 @@ func WireUp(ctx context.Context, declineAmount float32, tracer stdopentracing.Tr
 		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
 	}
 
-	fieldKeys := []string{"method"}
 	// Service domain.
 	var service Service
 	{
 		service = NewAuthorisationService(declineAmount)
 		service = LoggingMiddleware(logger)(service)
-		service = NewInstrumentingService(
-			kitprometheus.NewCounterFrom(
-				stdprometheus.CounterOpts{
-					Namespace: "http",
-					Subsystem: "requests",
-					Name:      "total",
-					Help:      "Number of requests received.",
-				},
-				fieldKeys),
-			kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-				Namespace: "http",
-				Subsystem: "request",
-				Name:      "duration_microseconds_sum",
-				Help:      "Total duration of requests in microseconds.",
-			}, fieldKeys),
-			service,
-		)
 	}
 
 	// Endpoint domain.
