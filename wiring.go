be9bf9fac44140a4ed074972524e206b117c9765
jpellizzari <jordan@weave.works>
2018-04-26 11:44:50 -0700

    Rename HTTP metrics

--- wiring.go
+++ wiring.go
@@ -14,10 +14,10 @@ import (
 
 var (
 	HTTPLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "request_duration_seconds",
+		Name:    "http_request_duration_seconds",
 		Help:    "Time (in seconds) spent serving HTTP requests.",
 		Buckets: prometheus.DefBuckets,
-	}, []string{"method", "route", "status_code", "isWS"})
+	}, []string{"method", "path", "status_code", "isWS"})
 )
 
 func init() {
